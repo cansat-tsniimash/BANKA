bbd03ae7162adab495b68a1e3cfb5ff484ecef3d
Nike_Alex <nikitaaleksey4@gmail.com>
2026-02-27 19:46:23 +0300

    добавлены функции для проверки

--- src/BANKA_construktor/app/bmp280/bmp280.c
+++ src/BANKA_construktor/app/bmp280/bmp280.c
@@ -9,33 +9,29 @@
 
 extern I2C_HandleTypeDef hi2c1;
 
-#define BMP280_ADDR (0x76)
+#define BMP280_ADDR (0x76 << 1)
 
 
 BME280_INTF_RET_TYPE bmp280_read_reg (uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
 {
-	HAL_I2C_Master_Transmit(&hi2c1, BMP280_ADDR, &reg_addr, 1, 100);
-	HAL_I2C_Master_Receive(&hi2c1, BMP280_ADDR, reg_data, len, 100);
-
+	if (HAL_I2C_Master_Transmit(&hi2c1, BMP280_ADDR, &reg_addr, 1, 100) != HAL_OK)
+		return -1;
+	if (HAL_I2C_Master_Receive(&hi2c1, BMP280_ADDR, reg_data, len, HAL_MAX_DELAY) != HAL_OK)
+		return -1;
 	return 0;
 }
-
-
-
 
 BME280_INTF_RET_TYPE bmp280_write_reg (uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
 {
 
 	uint8_t BMP_MASSIV [2] = {reg_addr, reg_data[0]};
 
-	for( int i=0; i < len; i++ )
+	for( uint32_t i = 0; i < len; i++ )
 	{
 		BMP_MASSIV [0] = reg_addr + i;
 		BMP_MASSIV [1] = reg_data[i];
-		HAL_I2C_Master_Seq_Transmit(&hi2c, BMP280_ADDR, BMP_MASSIV, 2, 100);
-	return 0;
+		if (HAL_I2C_Master_Transmit(&hi2c1, BMP280_ADDR, BMP_MASSIV, 2, 100) != HAL_OK)
+			return -1;
 	}
-
+	return 0;
 }
-
-
