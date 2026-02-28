6bbcd1c2dcf0abe3e38069c0f017e3d1015fe66d
Nike_Alex <nikitaaleksey4@gmail.com>
2026-02-28 19:53:08 +0300

    дописал БМПшку

--- src/BANKA_construktor/app/appmain.c
+++ src/BANKA_construktor/app/appmain.c
@@ -5,24 +5,53 @@
  *      Author: nikit
  */
 #include "stm32f1xx.h"
-#include "delay/dwt_delay.h"
+#include "ds18b20/ds18b20.h"
 #include "neo6mv2/neo6mv2.h"
+#include "bmp280/bmp280.h"
 
-
+extern I2C_HandleTypeDef hi2c1;
 extern UART_HandleTypeDef huart1;
+
+#define BMP280_ADDR (0x76 << 1)
 
 void app_main(void)
 {
-	/*dwt_delay_init();
-	uint32_t ds_stert_time = HAL_GetTick();
-	ds18b20_conv();*/
+	ds18b20_init(DS18B20_12_BIT);
+	uint32_t ds_start_time = HAL_GetTick();
+	ds18b20_conv();
 
 	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
 	__HAL_UART_ENABLE_IT(&huart1, UART_IT_ERR);
 	neo6mv2_Init();
 	GPS_Data gps_data;
+
+
+	struct bme280_dev bmp280;
+	bmp280.intf = BME280_I2C_INTF;
+	bmp280.read = bmp280_read_reg;
+	bmp280.write = bmp280_write_reg;
+	bmp280.delay_us = bmp280_delay_us;
+
+	bmp280.settings.osr_p = BME280_OVERSAMPLING_16X;
+	bmp280.settings.osr_t = BME280_OVERSAMPLING_16X;
+	bmp280.settings.filter = BME280_OVERSAMPLING_16X;
+	bmp280.settings.standby_time = BME280_STANDBY_TIME_20_MS;
+
+	bmp280_bus_t bmp_bus;
+	bmp_bus.ADDR = 0x76 << 1;
+	bmp_bus.hi2c = &hi2c1;
+	bmp280.intf_ptr = &bmp_bus;
+
+	bme280_init(&bmp280);
+	bme280_set_sensor_settings(BME280_ALL_SETTINGS_SEL, &bmp280);
+	bme280_set_sensor_mode(BME280_NORMAL_MODE, &bmp280);
+	struct bme280_data bmp_data;
+
+
 	while(1)
 	{
+		bme280_get_sensor_data(BME280_TEMP | BME280_PRESS, &bmp_data, &bmp280);
+
 		for (int i = 0; i <= 10; i++)
 		{
 			if (neo6mv2_work() == 1)
@@ -32,14 +61,13 @@ void app_main(void)
 		printf("cookie = %d\n", gps_data.cookie);
 		printf("%f %f\n",gps_data.latitude, gps_data.longitude);
 		printf("%d\n",gps_data.fixQuality);
-		//if ((HAL_GetTick() - ds_stert_time) > 750)
-		//{
-		//	volatile float temp = ds18b20_readtemp();
-		//	ds18b20_conv();
-		//	ds_stert_time = HAL_GetTick();
-		//}
 
-
+		if ((HAL_GetTick() - ds_start_time) > 750)
+		{
+			volatile float temp = ds18b20_readtemp();
+			ds18b20_conv();
+			ds_start_time = HAL_GetTick();
+		}
 
 
 		/*HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);*/
--- src/BANKA_construktor/app/bmp280/bmp280.c
+++ src/BANKA_construktor/app/bmp280/bmp280.c
@@ -5,33 +5,44 @@
  *      Author: nikit
  */
 #include "stm32f1xx.h"
-#include "bme280_defs.h"
+#include "delay/dwt_delay.h"
+#include "bmp280.h"
 
-extern I2C_HandleTypeDef hi2c1;
 
-#define BMP280_ADDR (0x76 << 1)
 
 
 BME280_INTF_RET_TYPE bmp280_read_reg (uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
 {
-	if (HAL_I2C_Master_Transmit(&hi2c1, BMP280_ADDR, &reg_addr, 1, 100) != HAL_OK)
-		return -1;
-	if (HAL_I2C_Master_Receive(&hi2c1, BMP280_ADDR, reg_data, len, HAL_MAX_DELAY) != HAL_OK)
-		return -1;
-	return 0;
+	bmp280_bus_t *ptr = (bmp280_bus_t *) intf_ptr;
+	HAL_StatusTypeDef read = HAL_I2C_Master_Transmit(ptr->hi2c, ptr->ADDR, &reg_addr, 1, 100);
+	if (read != HAL_OK)
+		return read;
+	read = HAL_I2C_Master_Receive(ptr->hi2c, ptr->ADDR, reg_data, len, 150);
+	if (read != HAL_OK)
+		return read;
+	return HAL_OK;
 }
 
 BME280_INTF_RET_TYPE bmp280_write_reg (uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
 {
-
-	uint8_t BMP_MASSIV [2] = {reg_addr, reg_data[0]};
+	bmp280_bus_t *ptr = (bmp280_bus_t *) intf_ptr;
+	uint8_t BMP_MASSIV[2];
+	HAL_StatusTypeDef read;
 
 	for( uint32_t i = 0; i < len; i++ )
 	{
 		BMP_MASSIV [0] = reg_addr + i;
 		BMP_MASSIV [1] = reg_data[i];
-		if (HAL_I2C_Master_Transmit(&hi2c1, BMP280_ADDR, BMP_MASSIV, 2, 100) != HAL_OK)
-			return -1;
+		read = HAL_I2C_Master_Transmit(ptr->hi2c, ptr->ADDR, BMP_MASSIV, 2, 100);
+		if (read != HAL_OK)
+		{
+			return read;
+		}
 	}
-	return 0;
+	return HAL_OK;
+}
+
+void bmp280_delay_us (uint32_t period, void *intf_ptr)
+{
+	dwt_delay_us(period);
 }
